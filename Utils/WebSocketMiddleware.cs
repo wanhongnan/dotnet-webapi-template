d628712041e65430f3948af1a9967a1fba1c85dc
south <south19831211@gmail.com>
2019-03-21 15:39:33 +0800

    WebSocketMiddleware update

--- Utils/WebSocketMiddleware.cs
+++ Utils/WebSocketMiddleware.cs
@@ -24,6 +24,8 @@ namespace Utils
     ///         return base.ProcessMessage("test1 : " + GetRouteValue("id") + receiveString);
     ///     }
     /// }
+    /// 
+    /// Microsoft.AspNetCore.WebSockets
     /// 在Startup.cs文件中添加
     ///        app.UseWebSockets();
     ///        app.UseMiddleware<TestWebSocketMiddleware>();
@@ -31,7 +33,7 @@ namespace Utils
     /// </summary>
     public abstract class WebSocketMiddleware
     {
-        private static List<WebSocket> _Sockets = new List<WebSocket>();
+        private static WebSocketTalbe sockTable = new WebSocketTalbe();
 
         RequestDelegate _next;
         RouteTemplate _routeTemplate;
@@ -82,7 +84,10 @@ namespace Utils
         {
             WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
             _WebSocket = webSocket;
-            lock (_Sockets) { _Sockets.Add(webSocket); }
+            lock (sockTable)
+            {
+                sockTable.Add(this, webSocket);
+            }
             const int maxMessageSize = 10240;
             var receivedDataBuffer = new ArraySegment<Byte>(new Byte[maxMessageSize]);
             var cancellationToken = context.RequestAborted;
@@ -109,7 +114,7 @@ namespace Utils
 
             try
             {
-                lock (_Sockets) { _Sockets.Remove(webSocket); }
+                lock (sockTable) { sockTable.Del(this, webSocket); }
                 await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, String.Empty, cancellationToken);
             }
             catch (Exception) { }
@@ -123,7 +128,7 @@ namespace Utils
                 if (_WebSocket != null)
                 {
                     var cancellationToken = new CancellationToken();
-                    lock (_Sockets) { _Sockets.Remove(_WebSocket); }
+                    lock (sockTable) { sockTable.Del(this, _WebSocket); }
                     await _WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, String.Empty, cancellationToken);
                 }
             }
@@ -135,21 +140,53 @@ namespace Utils
             return await Task.FromResult(receiveString);
         }
 
-        public static void SendMessageAll(string msg)
+        public async static Task SendMessageAll<T>(string msg)
+            where T : WebSocketMiddleware
         {
             List<WebSocket> sockets = new List<WebSocket>();
-            lock (_Sockets)
+            lock (sockTable)
             {
-                sockets = _Sockets.ToList();
+                sockets = sockTable.GetWebsockets(typeof(T));
             }
+            if (sockets.Count == 0) return;
 
             var cancellationToken = new CancellationToken();
-            foreach (var webSocket in sockets)
-            {
-                Byte[] bytes = System.Text.Encoding.UTF8.GetBytes(msg);
-                webSocket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, cancellationToken).Wait();
-            }
+            Byte[] bytes = System.Text.Encoding.UTF8.GetBytes(msg);
+            var tasks = sockets
+                .Select(f => f.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, cancellationToken));
+            await Task.WhenAll(tasks);
         }
     }
+
+    public class WebSocketTalbe
+    {
+        Dictionary<string, List<WebSocket>> _table = new Dictionary<string, List<WebSocket>>();
+        public void Add(WebSocketMiddleware mdw, WebSocket socket)
+        {
+            var type = mdw.GetType().ToString();
+            if (!_table.ContainsKey(type))
+                _table.Add(type, new List<WebSocket>());
+            var list = _table[type];
+            list.Add(socket);
+        }
+
+        public void Del(WebSocketMiddleware mdw, WebSocket socket)
+        {
+            var type = mdw.GetType().ToString();
+            if (!_table.ContainsKey(type))
+                _table.Add(type, new List<WebSocket>());
+            var list = _table[type];
+            list.Remove(socket);
+        }
+
+        public List<WebSocket> GetWebsockets(Type webSocketMiddlewareType)
+        {
+            var type = webSocketMiddlewareType.ToString();
+            if (!_table.ContainsKey(type))
+                _table.Add(type, new List<WebSocket>());
+            var list = _table[type];
+            return list;
+        }
+    }    
 }
 
